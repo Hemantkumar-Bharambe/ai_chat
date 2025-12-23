93bb977abd5fe20c078722fdacd45d4b07b253cc
Hemantkumar-Bharambe <hemant.bharambe@grawlixsoft.com>
2025-12-23 11:10:21 +0530

    final changes to web.php

--- resources/views/chat.blade.php
+++ resources/views/chat.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Grawlix UI</title>
+    <title>Grawlix</title>
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
